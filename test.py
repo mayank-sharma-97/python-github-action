e7998597c6aed59f9dd5f6da174c15873d3ee585
vinaymakam <vinay.makam@gmail.com>
2021-12-21 20:58:36 +0530

    initial commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-# import src.calculation as cal
+import src.calculation as cal
 
-# def test_add():
-#     assert cal.add(1,2) == 3
+def test_add():
+    assert cal.add(1,2) == 3
 
-# def test_sub():
-#     assert cal.sub(1,2) == 1
+def test_sub():
+    assert cal.sub(1,2) == 1
